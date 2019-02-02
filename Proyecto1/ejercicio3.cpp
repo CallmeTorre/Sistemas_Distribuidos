8f9f66e1c82917277073ca674294f1f5c6a6db1d
CallmeTorre <alexis_torreblanca@outlook.com>
2019-02-01 19:23:05 -0600

    Ejercicio 3 del Proyecto 1 agregado

---
+++ Proyecto1/ejercicio3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+const double GRAVITY = 9.81;
+
+int main(){
+    double tiempo;
+    int horas, minutos, segundos;
+
+    cout << "Bienvenido." << endl;
+    cout << "Pasaremos de segundos a un formato H M S." << endl;
+    cout << "Inserte los segundos a transformar: ";
+    cin >> tiempo;
+
+    horas = tiempo / 3600;
+    minutos = (tiempo - (horas * 3600)) / 60;
+    segundos = tiempo - ((horas * 3600) + (minutos * 60));
+
+    cout << "Como resultado tenemos: ";
+    cout << horas << " horas " << minutos << " minutos " << segundos << " segundos." << endl;
+}
