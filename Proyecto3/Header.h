66ed884136ca8f72bd6353eb4c01645f0e8b19d2
CallmeTorre <alexis_torreblanca@outlook.com>
2019-04-11 00:25:48 -0500

    Agregado Header.h2

--- Proyecto3/Header.h
+++ Proyecto3/Header.h
@@ -1,10 +1,27 @@
-#ifndef MENSAJE_H
-#define MENSAJE_H
+#ifndef HEADER_H
+#define HEADER_H
 
 #define MAX_PATH 255
 #define BUF_SIZE 1024
 #define FILE_SERVER 243
 #define GET 1
 #define POST 2
+#define OK 0
+#define E_BAD_OPCODE -1
+#define E_BAD_PARAM -2
+#define E_IO -3
 
+struct mensaje{
+    long source;
+    long dest;
+    long opcode;
+    long count;
+    long offset;
+
+    long extra_1;
+    long extra_2;
+    long result;
+    char name[MAX_PATH];
+    char data[BUF_SIZE];
+};
 #endif
