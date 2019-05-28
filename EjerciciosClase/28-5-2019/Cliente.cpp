3ae48a64d180678726b6df37e93cdb587e88ca4f
CallmeTorre <alexis_torreblanca@outlook.com>
2019-05-28 11:09:35 -0500

    Agregado ejercicio hoy

---
+++ EjerciciosClase/28-5-2019/Cliente.cpp
@@ -0,0 +1,19 @@
+#include "SocketDatagrama.h"
+
+using namespace std;
+
+int main(int argc, char const *argv[]){
+	SocketDatagrama socket = SocketDatagrama(0);
+    int nums[2];
+    nums[0] = 5;
+    nums[1] = 3;
+
+    PaqueteDatagrama datagramaEnvia = PaqueteDatagrama((char *) nums, 2 * sizeof(int),"127.0.0.1", 7300);
+    socket.envia(datagramaEnvia);
+
+    PaqueteDatagrama datagramaRecibe =  PaqueteDatagrama(sizeof(int));
+    socket.recibe(datagramaRecibe);
+    int * res = (int * ) datagramaRecibe.obtieneDatos();
+    cout << nums[0] << " + " << nums[1] << " = " << *res <<endl;
+    return 0;
+}
