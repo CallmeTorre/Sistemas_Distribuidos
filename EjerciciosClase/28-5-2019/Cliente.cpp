4573abae281e0dd4650b1eb9ffb5c3fd937190b3
CallmeTorre <alexis_torreblanca@outlook.com>
2019-05-28 11:55:13 -0500

    Added receive ips

--- EjerciciosClase/28-5-2019/Cliente.cpp
+++ EjerciciosClase/28-5-2019/Cliente.cpp
@@ -8,12 +8,15 @@ int main(int argc, char const *argv[]){
     nums[0] = 5;
     nums[1] = 3;
 
-    PaqueteDatagrama datagramaEnvia = PaqueteDatagrama((char *) nums, 2 * sizeof(int),"127.0.0.1", 7300);
+    PaqueteDatagrama datagramaEnvia = PaqueteDatagrama((char *) nums, 2 * sizeof(int),"10.100.95.255", 7300);
     socket.envia(datagramaEnvia);
-
     PaqueteDatagrama datagramaRecibe =  PaqueteDatagrama(sizeof(int));
-    socket.recibe(datagramaRecibe);
-    int * res = (int * ) datagramaRecibe.obtieneDatos();
-    cout << nums[0] << " + " << nums[1] << " = " << *res <<endl;
+    for(;;){
+        socket.recibe(datagramaRecibe);
+        int * res = (int * ) datagramaRecibe.obtieneDatos();
+        cout << "Cliente: " << datagramaRecibe.obtieneDireccion() << endl;
+        cout << "Puerto: " << datagramaRecibe.obtienePuerto() << endl;
+        cout << nums[0] << " + " << nums[1] << " = " << *res <<endl;
+    }
     return 0;
 }
