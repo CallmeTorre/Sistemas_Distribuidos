9fe10f0de7b3a4808d75f92561677e926b9819e1
CallmeTorre <alexis_torreblanca@outlook.com>
2019-05-30 21:52:55 -0500

    Practica final concluida

--- EjerciciosClase/28-5-2019/Cliente.cpp
+++ EjerciciosClase/28-5-2019/Cliente.cpp
@@ -1,4 +1,5 @@
 #include "SocketDatagrama.h"
+#include <chrono>
 
 using namespace std;
 
@@ -9,18 +10,17 @@ int main(int argc, char const *argv[]){
     nums[1] = 3;
 
     PaqueteDatagrama datagramaEnvia = PaqueteDatagrama((char *) nums, 2 * sizeof(int),"10.100.95.255", 7300);
+    auto ini = chrono::high_resolution_clock::now();
+
     socket.envia(datagramaEnvia);
     PaqueteDatagrama datagramaRecibe =  PaqueteDatagrama(sizeof(int));
-    for(;;){
-        if(socket.recibe(datagramaRecibe) > 0){
-            int * res = (int * ) datagramaRecibe.obtieneDatos();
-            cout << "Cliente: " << datagramaRecibe.obtieneDireccion() << endl;
-            cout << "Puerto: " << datagramaRecibe.obtienePuerto() << endl;
-            cout << nums[0] << " + " << nums[1] << " = " << *res <<endl;
-        }
-        else{
-            break;
-        }
+    while(socket.recibe(datagramaRecibe) > 0){
+        auto fin = chrono::high_resolution_clock::now();
+        int * res = (int * ) datagramaRecibe.obtieneDatos();
+        cout << "Cliente: " << datagramaRecibe.obtieneDireccion() << endl;
+        cout << "Puerto: " << datagramaRecibe.obtienePuerto() << endl;
+        cout << nums[0] << " + " << nums[1] << " = " << *res <<endl;
+        cout << "Latencia: " << chrono::duration_cast<chrono::nanoseconds>(fin- ini).count() << " ms" <<endl;
     }
     return 0;
 }
